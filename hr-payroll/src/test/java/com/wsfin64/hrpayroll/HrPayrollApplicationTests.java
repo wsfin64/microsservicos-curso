387ec6604da370114681d1292b2c2f3deefbf272
Wellington da Silva <wsfin64@gmail.com>
2021-10-13 19:41:10 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/wsfin64/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsfin64.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
