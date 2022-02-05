24343f44f335f4a8d02e588cc6d2b1fe6becc273
Wellington <wsfin64@gmail.com>
2022-02-05 10:26:26 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/wsfin64/hrpayroll/entities/WorkerRequest.java
@@ -0,0 +1,62 @@
+package com.wsfin64.hrpayroll.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class WorkerRequest implements Serializable {
+	 
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	
+	private String name;
+	private Double dailyIncome; 
+	
+	public WorkerRequest() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getDailyIncome() {
+		return dailyIncome;
+	}
+
+	public void setDailyIncome(Double dailyIncome) {
+		this.dailyIncome = dailyIncome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WorkerRequest other = (WorkerRequest) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
--- hr-payroll/src/main/java/com/wsfin64/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/wsfin64/hrpayroll/services/PaymentService.java
@@ -1,14 +1,33 @@
 package com.wsfin64.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.wsfin64.hrpayroll.entities.Payment;
 
+import com.wsfin64.hrpayroll.entities.WorkerRequest;
+
 @Service
 public class PaymentService {
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
 
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", String.valueOf(workerId));
+		
+		WorkerRequest worker = restTemplate.getForObject(workerHost + "/workers/{id}", WorkerRequest.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
