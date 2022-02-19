13b210af20653ef629b86bd4ff97aac248ecabf3
Wellington <wsfin64@gmail.com>
2022-02-19 11:52:26 -0300

    Configuring Eureka Server

--- hr-eureka-server/src/main/java/com/wsfin64/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/wsfin64/hreurekaserver/HrEurekaServerApplication.java
@@ -2,7 +2,11 @@ package com.wsfin64.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import jdk.jfr.Enabled;
+
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
