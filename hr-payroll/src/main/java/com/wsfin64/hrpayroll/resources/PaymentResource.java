c9c7350c293142b03550928f3f2cce92f6fdce85
Wellington <wsfin64@gmail.com>
2022-02-20 22:44:20 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/wsfin64/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/wsfin64/hrpayroll/resources/PaymentResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.wsfin64.hrpayroll.entities.Payment;
 import com.wsfin64.hrpayroll.entities.WorkerResponse;
 import com.wsfin64.hrpayroll.services.PaymentService;
@@ -20,6 +21,7 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService paymentService;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = paymentService.getPayment(workerId, days);
@@ -29,6 +31,11 @@ public class PaymentResource {
 	@GetMapping("/workers")
 	public ResponseEntity<List<WorkerResponse>> getWorkers(){
 		return ResponseEntity.ok(paymentService.getWorkers());
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+		Payment payment = new Payment("Brann", 400.0, days);
+		return ResponseEntity.ok(payment);
 	}
 	
 	
--- hr-worker/src/main/java/com/wsfin64/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/wsfin64/hrworker/resources/WorkerResource.java
@@ -38,6 +38,14 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findbyId(@PathVariable("id") Long id){
 		
+		// Forçando um timeout
+//		try {
+//			Thread.sleep(3000L);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		return ResponseEntity.ok(repository.findById(id).get());
