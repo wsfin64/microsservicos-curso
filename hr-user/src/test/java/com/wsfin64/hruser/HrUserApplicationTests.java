07821fbaf3e49147f898995302091343f238a20b
Wellington <wsfin64@gmail.com>
2022-02-28 17:08:09 -0300

    Project hr-user created

---
+++ hr-user/src/test/java/com/wsfin64/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsfin64.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
