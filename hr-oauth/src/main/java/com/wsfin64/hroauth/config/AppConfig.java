aa0f96a6fcf6f0fc3a2103bfa2f5975bc0d77a48
Wellington <wsfin64@gmail.com>
2022-03-01 17:32:48 -0300

    Login, JWT token

---
+++ hr-oauth/src/main/java/com/wsfin64/hroauth/config/AppConfig.java
@@ -0,0 +1,33 @@
+package com.wsfin64.hroauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    // Criando chave secreta da aplicação
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey("MY-SECRET-KEY");
+        return tokenConverter;
+    }
+
+    @Bean
+    public JwtTokenStore tokenStore(){
+        return new JwtTokenStore((accessTokenConverter()));
+    }
+
+
+
+}
