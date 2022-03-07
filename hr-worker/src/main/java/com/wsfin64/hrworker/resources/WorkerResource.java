0ad8192950d2ca972a1a803c91a02c88b94c5244
Wellington <wsfin64@gmail.com>
2022-03-07 20:44:16 -0300

    db config server

--- hr-worker/src/main/java/com/wsfin64/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/wsfin64/hrworker/resources/WorkerResource.java
@@ -26,8 +26,8 @@ public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
-	@Value("${test.config}")
-	private String testConfig;
+//	@Value("${test.config}")
+//	private String testConfig;
 	
 	@Autowired
 	private Environment env;
@@ -42,7 +42,7 @@ public class WorkerResource {
 
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs(){
-		logger.info("CONFIG = " + testConfig);
+		//logger.info("CONFIG = " + testConfig);
 		return ResponseEntity.noContent().build();
 	}
 	
